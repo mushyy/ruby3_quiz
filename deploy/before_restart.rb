d9d5752669746db5b553058c66bc75d394428a38
Callum Pease <cpease1997@hotmail.com>
2021-07-11 15:29:02 +0100

    Update before_restart.rb

--- deploy/before_restart.rb
+++ deploy/before_restart.rb
@@ -1,3 +1,4 @@
-on_app_master do
-  sudo "echo on_app_master"
+on_app_servers_and_utilities do
+  run "sed -i '/DD_VERSION/d'  #{config.shared_path}/config/env.cloud"
+  run ". #{config.shared_path}/config/env.cloud && echo 'export DD_VERSION=\"#{config.active_revision}\"' >> /data/ruby3_quiz/shared/config/env.cloud"
 end
