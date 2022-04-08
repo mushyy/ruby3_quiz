6961690f7904778ec566488baffa64567839151e
Callum Pease <cpease@engineyard.com>
2022-04-08 07:20:06 +0100

    Update for no database

--- deploy/before_restart.rb
+++ deploy/before_restart.rb
@@ -1,5 +1,2 @@
-on_app_servers_and_utilities do
-  run "sed -i '/DD_VERSION/d'  #{config.shared_path}/config/env.cloud"
-  run ". #{config.shared_path}/config/env.cloud && echo 'export DD_VERSION=\"#{config.active_revision}\"' >> #{config.shared_path}/config/env.cloud"
-  run ". #{config.shared_path}/config/env.cloud && curl -X PUT https://api.engineyard.com/environment_variables/4280 --header \"Content-Type: application/json\" --header \"Accept: application/vnd.engineyard.v3+json\" --data '{ \"environment_variable\": { \"name\": \"DD_VERSION\", \"value\": \"#{config.active_revision}\" } }' --header \"X-EY-TOKEN: $APITOKENEY \""
-end
+run "apt-get install -y libsqlite3-dev && apt-get install -y libsqlite-dev"
+run "cd #{config.release_path} && bundle exec rails db:migrate && bundle exec rails db:seed"
